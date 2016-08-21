2220af27f2b2ac3476615c837829a0e27923c4d9
MayOliver <schwelmer6@gmail.com>
2016-08-21 20:33:57 +0200

    Complete Programm
    Feel free to use :)

---
+++ PraxisProjectIuK/ChangeTrc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PraxisProjectIuK
+{
+    public class ChangeTrc
+    {
+        public string myfile { get; set; }
+        public string filename { get; set; }
+        public void changeExtensions()
+        {
+            string txtFileName = @"Output/kinectSkeleton.txt";
+
+            
+            File.Move(txtFileName, Path.ChangeExtension(txtFileName , ".trc"));
+
+        }
+        
+    }
+}
---
+++ PraxisProjectIuK/Streamerstellung.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PraxisProjectIuK
+{
+     public static class Streamerstellung
+    {
+        public static ImageSource ToBitmap(this ColorFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            byte[] pixels = new byte[width * height * ((format.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+    }
+}
